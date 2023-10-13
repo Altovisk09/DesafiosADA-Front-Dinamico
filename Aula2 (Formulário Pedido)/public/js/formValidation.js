5bf1520e28c3a19fa5bd26031a505f9e0fc2aba5
Altovisk <ericaltao13@gmail.com>
2023-10-13 18:24:50 -0300

    estrutura corrigida

--- Aula2 (Formulário Pedido)/public/js/formValidation.js
+++ Aula2 (Formulário Pedido)/public/js/formValidation.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', function(){
     const form = document.forms[0];
-    const inputs = ['order', 'date', 'amount', 'email', 'delivery-prediction']
     
     class FormError extends Error{
         constructor(message, input, value){
@@ -33,17 +32,34 @@ window.addEventListener('DOMContentLoaded', function(){
     }
     
     const validaInputs = (input, value) => {
+        if (input === "order" && !validaVazio(value))
+        throw new FormError("Pedido inválido", input, value);
 
-    }
+      if (input === "data" && !validaDate(new Date(value)))
+        throw new FormError("Data inválida", input, value);
+
+      if (input === "amount" && !validaAmount(value))
+        throw new FormError("Valor inválido", input, value);
+
+      if (input === "email" && !validaEmail(value))
+        throw new FormError("Email inválido", input, value);
+
+      if (input === "delivery" && !validaDelivery(value))
+        throw new FormError("Previsão inválida", input, value);
+    };
 
 
     form.addEventListener('submit', (event)=>{
         event.preventDefault();
-        
+        const inputs = ['order', 'date', 'amount', 'email', 'delivery-prediction']
         try{
+            inputs.forEach((input) => {
+                const inputValue = form[input].value;
 
+                validaInputs(input, inputValue)
+            })
         }catch(error){
-
+            alert(error.message);
         }
     })
 })
